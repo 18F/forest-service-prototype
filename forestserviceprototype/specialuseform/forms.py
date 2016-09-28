0963cb74fed06657c14bf7ed55802976fd47770f
Laura Gerhardt <laura.gerhardt@gsa.gov>
2016-09-28 14:03:42 -0400

    attempt 1

--- forestserviceprototype/specialuseform/forms.py
+++ forestserviceprototype/specialuseform/forms.py
@@ -2,14 +2,19 @@
 from localflavor.us import forms as localflavor
 from .models import Permit
 import floppyforms.__future__ as forms
+from form_utils.forms import BetterForm
 
-class PermitForm(forms.ModelForm):
+class PermitForm(BetterForm):
     class Meta:
         model = Permit
-        fields = ['event_name', 'organizer_address_1', 'organizer_address_2', 'city', 'state', 'zipcode', 'email', 'phone_daytime', 'phone_evening', 'description', 'location', 'participant_number', 'spectator_number', 'start_date', 'end_date', 'permit_holder_name', 'permit_holder_signature']
+        # fields = ['event_name', 'organizer_address_1', 'organizer_address_2', 'city', 'state', 'zipcode', 'email', 'phone_daytime', 'phone_evening', 'description', 'location', 'participant_number', 'spectator_number', 'start_date', 'end_date', 'permit_holder_name', 'permit_holder_signature']
+        fieldsets = [
+        ('test', {
+                'fields': ['event_name', 'organizer_address_1'],
+                'legend': 'a test legend',
+            })
+        ]
         widgets = {
-#            'start_date': forms.SelectDateWidget,
-#            'end_date': forms.SelectDateWidget,
             'phone_daytime': forms.PhoneNumberInput,
             'phone_evening': forms.PhoneNumberInput
         }
