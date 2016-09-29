296983f7cf80a22a1b55a37ac7dab5280fbd1e3c
Laura Gerhardt <laura.gerhardt@gsa.gov>
2016-09-29 11:48:57 -0400

    add front end styling

--- forestserviceprototype/specialuseform/forms.py
+++ forestserviceprototype/specialuseform/forms.py
@@ -2,19 +2,43 @@
 from localflavor.us import forms as localflavor
 from .models import Permit
 import floppyforms.__future__ as forms
-from form_utils.forms import BetterForm
+from form_utils.forms import BetterModelForm
 
-class PermitForm(BetterForm):
+class PermitForm(BetterModelForm):
     class Meta:
         model = Permit
-        # fields = ['event_name', 'organizer_address_1', 'organizer_address_2', 'city', 'state', 'zipcode', 'email', 'phone_daytime', 'phone_evening', 'description', 'location', 'participant_number', 'spectator_number', 'start_date', 'end_date', 'permit_holder_name', 'permit_holder_signature']
         fieldsets = [
-        ('test', {
-                'fields': ['event_name', 'organizer_address_1'],
-                'legend': 'a test legend',
-            })
+        ('general', {
+                'fields': ['event_name', 'organizer_address_1', 'organizer_address_2', 'city', 'state', 'zipcode', 'email', 'phone_daytime', 'phone_evening'],
+                'legend': 'Organizer Information',
+            }),
+        ('event_details', {
+                'fields': ['description', 'location', 'participant_number', 'spectator_number', 'start_date', 'end_date'],
+                'legend': 'Event Details',
+            }),
+        ('primary_permit_holder', {
+                'fields': ['permit_holder_name', 'permit_holder_address_1', 'permit_holder_address_2', 'permit_holder_city', 'permit_holder_state', 'permit_holder_zipcode', 'permit_holder_signature'],
+                'legend': 'Primary Permit Holder Information',
+                'classes': ['permit_holder_1_fieldset']
+            }),
+        ('secondary_permit_holder', {
+                'fields': ['permit_holder_2_name', 'permit_holder_2_address_1', 'permit_holder_2_address_2', 'permit_holder_2_city', 'permit_holder_2_state', 'permit_holder_2_zipcode', 'permit_holder_2_signature'],
+                'legend': 'Secondary Permit Holder Information',
+                'classes': ['permit_holder_fieldset', 'permit_hide']
+            }),
         ]
         widgets = {
-            'phone_daytime': forms.PhoneNumberInput,
-            'phone_evening': forms.PhoneNumberInput
+            'city': forms.TextInput(attrs={'class': 'medium-grid'}),
+            'state': forms.Select(attrs={'class': 'small-grid'}),
+            'zipcode': forms.TextInput(attrs={'class': 'usa-input-medium'}),
+            'phone_daytime': forms.PhoneNumberInput(attrs={'class': 'medium-grid'}),
+            'phone_evening': forms.PhoneNumberInput(attrs={'class': 'medium-grid'}),
+            'start_date': forms.DateInput(attrs={'class': 'medium-grid'}),
+            'end_date': forms.DateInput(attrs={'class': 'medium-grid'}),
+            'permit_holder_city': forms.TextInput(attrs={'class': 'medium-grid'}),
+            'permit_holder_state': forms.Select(attrs={'class': 'small-grid'}),
+            'permit_holder_zipcode': forms.TextInput(attrs={'class': 'usa-input-medium'}),
+            'permit_holder_2_city': forms.TextInput(attrs={'class': 'medium-grid'}),
+            'permit_holder_2_state': forms.Select(attrs={'class': 'small-grid'}),
+            'permit_holder_2_zipcode': forms.TextInput(attrs={'class': 'usa-input-medium'}),
         }
---
+++ forestserviceprototype/static_assets/additional.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  $('.add_another_permit_holder').click(function(e){
+    e.preventDefault();
+    $(".permit_hide").toggle();
+    $(this).text($(this).text() == "Add Another Permit Holder" ? "Remove Second Permit Holder" : "Add Another Permit Holder");
+  });
+
+  const fieldMapping = {
+    'id_organizer_address_1' : 'id_permit_holder_address_1',
+    'id_organizer_address_2' : 'id_permit_holder_address_2',
+    'id_city' : 'id_permit_holder_city',
+    'id_state' : 'id_permit_holder_state',
+    'id_zipcode' : 'id_permit_holder_zipcode',
+  }
+
+  function addEventListener(listener, outputfield){
+    $('#'+listener).on('input', function() {
+      if($('#permit-holder-same-address').val() == 'yes'){
+          $('#'+outputfield).val($(this).val());
+      }
+    });
+
+  }
+  //Initialize to disabled
+  for(key in fieldMapping){
+    $('#'+fieldMapping[key]).prop('disabled', true);
+
+    //Add listeners
+    addEventListener(key, fieldMapping[key]);
+  };
+
+  $('#permit-holder-same-address').change(function(){
+    console.log($(this).val());
+    if($(this).val() == "no"){
+    console.log($("#permit_holder_1_fieldset input").val())
+      $('.permit_holder_1_fieldset input').prop('disabled', false);
+      $('.permit_holder_1_fieldset select').prop('disabled', false);
+    } else {
+      for(key in fieldMapping){
+        $('#'+fieldMapping[key]).val($('#'+key).val()).prop('disabled', true);
+      };
+    }
+
+  });
+});
