150d5d5a1a987b9e133f015d5c82f09fbec0ec98
stvnrlly <steven.reilly@gsa.gov>
2016-12-09 12:23:22 -0500

    pep8-ify

--- forestserviceprototype/specialuseform/models.py
+++ forestserviceprototype/specialuseform/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from localflavor.us import models as localflavor
 
+
 # Create your models here.
 class Permit(models.Model):
     STATUS_CHOICES = (
@@ -11,41 +12,117 @@ class Permit(models.Model):
         ('in_review', 'In Review'),
         ('not_approved', 'Not Approved')
     )
-    event_name = models.CharField(max_length=250,
-        help_text='The name of the event')
+
+    event_name = models.CharField(
+        max_length=250,
+        help_text='The name of the event'
+    )
     organizer_address_1 = models.CharField(max_length=250)
     organizer_address_2 = models.CharField(max_length=250)
-    city = models.CharField(max_length=250)
-    state = localflavor.USStateField(default="OH")
+    city = models.CharField(
+        max_length=250
+    )
+    state = localflavor.USStateField(
+        default="OH"
+    )
     zipcode = localflavor.USZipCodeField()
     phone_daytime = localflavor.PhoneNumberField()
-    phone_evening = localflavor.PhoneNumberField(blank=True)
+    phone_evening = localflavor.PhoneNumberField(
+        blank=True
+    )
     description = models.TextField()
-    location = models.CharField(max_length=250)
-    participant_number = models.IntegerField(verbose_name='Number of Participants', help_text='This is the number of people who will directly participate in the event.')
-    spectator_number = models.IntegerField(blank=True, verbose_name='Number of Spectators', help_text='If your event will have spectators (such as at a sporting event), please note how many additional people will be spectators.')
-    start_date = models.DateTimeField(default=timezone.now, help_text='Format: MM/DD/YYYY')
-    end_date = models.DateTimeField(default=timezone.now, help_text='Format: MM/DD/YYYY')
-    permit_holder_name = models.CharField(max_length=250, help_text='Name of Permit Holder')
-    permit_holder_signature = models.ImageField(blank=True)
-    permit_holder_address_1 = models.CharField(max_length=250, verbose_name="Street Address 1")
-    permit_holder_address_2 = models.CharField(max_length=250, blank=True, verbose_name="Street Address 2")
-    permit_holder_city = models.CharField(max_length=250, verbose_name="City")
-    permit_holder_state = localflavor.USStateField(verbose_name="State")
-    permit_holder_zipcode = localflavor.USZipCodeField(verbose_name="Zipcode")
-    permit_holder_2_name = models.CharField(max_length=250, help_text='Name of Permit Holder', blank=True)
-    permit_holder_2_signature = models.ImageField(blank=True)
-    permit_holder_2_address_1 = models.CharField(max_length=250, verbose_name="Street Address 1", blank=True)
-    permit_holder_2_address_2 = models.CharField(max_length=250, blank=True, verbose_name="Street Address 2")
-    permit_holder_2_city = models.CharField(max_length=250, verbose_name="City", blank=True)
-    permit_holder_2_state = localflavor.USStateField(verbose_name="State", blank=True)
-    permit_holder_2_zipcode = localflavor.USZipCodeField(verbose_name="Zipcode", blank=True)
-    created = models.DateTimeField(default=timezone.now)
-    updated = models.DateTimeField(default=timezone.now)
-    status = models.CharField(max_length=100,
-                              choices=STATUS_CHOICES,
-                              default='needs_approval')
-    decision_explanation = models.TextField(blank=True)
+    location = models.CharField(
+        max_length=250)
+    participant_number = models.IntegerField(
+        verbose_name='Number of Participants',
+        help_text='This is the number of people who will directly participate '
+                  'in the event.'
+    )
+    spectator_number = models.IntegerField(
+        blank=True,
+        verbose_name='Number of Spectators',
+        help_text='If your event will have spectators (such as at a sporting '
+                  'event), please note how many additional people will be '
+                  'spectators.'
+    )
+    start_date = models.DateTimeField(
+        default=timezone.now,
+        help_text='Format: MM/DD/YYYY'
+    )
+    end_date = models.DateTimeField(
+        default=timezone.now,
+        help_text='Format: MM/DD/YYYY'
+    )
+    permit_holder_name = models.CharField(
+        max_length=250,
+        help_text='Name of Permit Holder'
+    )
+    permit_holder_signature = models.ImageField(
+        blank=True
+    )
+    permit_holder_address_1 = models.CharField(
+        max_length=250,
+        verbose_name="Street Address 1"
+    )
+    permit_holder_address_2 = models.CharField(
+        max_length=250,
+        blank=True,
+        verbose_name="Street Address 2"
+    )
+    permit_holder_city = models.CharField(
+        max_length=250,
+        verbose_name="City"
+    )
+    permit_holder_state = localflavor.USStateField(
+        verbose_name="State"
+    )
+    permit_holder_zipcode = localflavor.USZipCodeField(
+        verbose_name="Zipcode"
+    )
+    permit_holder_2_name = models.CharField(
+        max_length=250,
+        help_text='Name of Permit Holder',
+        blank=True
+    )
+    permit_holder_2_signature = models.ImageField(
+        blank=True
+    )
+    permit_holder_2_address_1 = models.CharField(
+        max_length=250,
+        verbose_name="Street Address 1",
+        blank=True
+    )
+    permit_holder_2_address_2 = models.CharField(
+        max_length=250,
+        blank=True,
+        verbose_name="Street Address 2"
+    )
+    permit_holder_2_city = models.CharField(
+        max_length=250,
+        verbose_name="City",
+        blank=True
+    )
+    permit_holder_2_state = localflavor.USStateField(
+        verbose_name="State",
+        blank=True
+    )
+    permit_holder_2_zipcode = localflavor.USZipCodeField(
+        verbose_name="Zipcode",
+        blank=True
+    )
+    created = models.DateTimeField(
+        default=timezone.now
+    )
+    updated = models.DateTimeField(
+        default=timezone.now
+    )
+    status = models.CharField(
+        max_length=100,
+        choices=STATUS_CHOICES,
+        default='needs_approval'
+    )
+    decision_explanation = models.TextField(
+        blank=True)
 
     def __str__(self):
         return self.event_name
--- forestserviceprototype/specialuseform/views.py
+++ forestserviceprototype/specialuseform/views.py
@@ -12,10 +12,13 @@ import json
 
 # Create your views here.
 
+
 def home(request):
     return render(request, "specialuseform/home.html")
 
-def submit(request, permit_id=None, template_name='specialuseform/submit.html'):
+
+def submit(request, permit_id=None,
+           template_name='specialuseform/submit.html'):
     if permit_id:
         permit = get_object_or_404(Permit, permit_id=permit_id)
         submit_button_text = 'Save Changes'
@@ -31,8 +34,8 @@ def submit(request, permit_id=None, template_name='specialuseform/submit.html'):
         # Send the user a confirmation message
         send_mail(
             subject='Application Submitted',
-            message='Your application for {0} has been received, and is #{1}. To'
-            'view its status or make changes, please visit forest-service-'
+            message='Your application for {0} has been received, and is #{1}. '
+            'To view its status or make changes, please visit forest-service-'
             'prototype.fr.cloud.gov/submitted/{1}'.format(
                 form.instance.event_name,
                 form.instance.permit_id,
@@ -65,11 +68,19 @@ def submit(request, permit_id=None, template_name='specialuseform/submit.html'):
         'form': form, 'submit_text': submit_button_text
     })
 
+
 def submitted_permit(request, permit_id):
     check_status = False if request.GET.get('new') else True
     permit = get_object_or_404(Permit.objects.filter(permit_id=permit_id))
     permit_dict = PermitForm(data=model_to_dict(permit))
-    return render(request, "specialuseform/submitted_permit.html", {'permit': permit, 'permit_dict': permit_dict, 'check_status': check_status})
+    return render(request,
+                  "specialuseform/submitted_permit.html",
+                  {'permit': permit,
+                   'permit_dict': permit_dict,
+                   'check_status': check_status
+                   }
+                  )
+
 
 def change_application_status(request, permit_id, status):
     decision_explanation = request.POST.get('deny_reason')
@@ -93,19 +104,26 @@ def change_application_status(request, permit_id, status):
         fail_silently=False
     )
 
-    return HttpResponse( json.dumps({"status": permit.status, "reason": permit.decision_explanation}),
-        content_type="application/json"
+    json_response = json.dumps(
+        {"status": permit.status, "reason": permit.decision_explanation}
     )
+
+    return HttpResponse(json_response, content_type="application/json")
+
 
 def cancel(request, permit_id):
     permit = get_object_or_404(Permit.objects.filter(permit_id=permit_id))
     permit.status = 'user_cancelled'
     permit.save()
-    return render(request, 'specialuseform/cancel_permit.html', {'permit': permit})
+    return render(request, 'specialuseform/cancel_permit.html',
+                  {'permit': permit})
+
 
 def print_permit(request, permit_id):
     permit = get_object_or_404(Permit.objects.filter(permit_id=permit_id))
-    return render(request, 'specialuseform/print_permit.html', {'permit': permit})
+    return render(request, 'specialuseform/print_permit.html',
+                  {'permit': permit})
+
 
 # @todo: Fix redirect so users don't get stuck in admin screen. See #7.
 @login_required(login_url='/admin/')
