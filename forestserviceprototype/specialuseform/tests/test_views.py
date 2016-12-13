d730b065a5f8b04f64375d51206315085952e716
stvnrlly <steven.reilly@gsa.gov>
2016-12-13 11:52:50 -0500

    test form submission view

--- forestserviceprototype/specialuseform/tests/test_views.py
+++ forestserviceprototype/specialuseform/tests/test_views.py
@@ -1,5 +1,6 @@
 import pytest
 from django.contrib.auth.models import AnonymousUser
+from django.utils import timezone
 from specialuseform import views
 from specialuseform.factories import PermitFactory
 
@@ -10,10 +11,39 @@ def test_home(rf):
     assert response.status_code == 200
 
 
-def test_submit_view(rf):
+def test_submit_get(rf):
     request = rf.get('/submit')
     response = views.submit(request)
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_submit_post(rf):
+    # permit = PermitFactory()
+    from django.core import mail
+    request = rf.post('/submit/', data={
+        'event_name': 'hi',
+        'organizer_address_1': '123 Forest Lane',
+        'city': 'Treeville',
+        'state': 'CO',
+        'zipcode': 12345,
+        'phone_daytime': '555-555-5555',
+        'description': 'wow. such permit. very event.',
+        'location': 'a park',
+        'participant_number': 1000,
+        'spectator_number': 5000,
+        'start_date': '2016-12-12',
+        'end_date': '2016-12-12',
+        'permit_holder_name': 'I. M. User',
+        'permit_holder_address_1': '456 User Avenue',
+        'permit_holder_city': 'Testville',
+        'permit_holder_state': 'CO',
+        'permit_holder_zipcode': 54321,
+        'email': 'user@notawebsite.gov'
+    })
+    response = views.submit(request)
+    assert len(mail.outbox) == 1
+    assert response.status_code == 302
 
 
 @pytest.mark.django_db
